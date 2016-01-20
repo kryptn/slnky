16893c99e64cf85ef2ce726635196de75ab7fcde
kryptn <kryptn@gmail.com>
2016-01-20 01:59:59 +0000

    GET endpoint working

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ api = Api(app)
 
 class Links(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    slug = db.Column(db.String, unique=True)
+    #slug = db.Column(db.String, unique=True)
     url = db.Column(db.String)
 
     def __init__(self, url):
@@ -26,6 +26,36 @@ class Links(db.Model):
             db.session.commit()
 
         return result, created
+
+class Slnky(Resource):
+    def get(self, redirect_id):
+        rurl = None
+        try:
+            result = Links.query.get_or_404(redirect_id)
+            rurl = result.url
+        except NotFound:
+            pass
+
+        return {'redirect_id':rurl, 'redirect': True}
+
+    def put(self, url):
+        result, created = Links.get_or_create(url)
+        return {'redirect_id': result.id,
+                'redirect_url': result.url,
+                'redirect': False}
+
+api.add_resource(Slnky, '/<int:redirect_id>', '/c/<string:url>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+
+
+
+
+
+
 
 
 
