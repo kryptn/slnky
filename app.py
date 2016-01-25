2c9c9106229b8d052b01a5bc5bf9dd038939f51b
kryptn <kryptn@gmail.com>
2016-01-25 01:11:07 +0000

    Added method for non-id slugs

--- app.py
+++ app.py
@@ -4,85 +4,96 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///slnky.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  # to suppress an obnoxious warning
+app.config['ERROR_404_HELP'] = False
 db = SQLAlchemy(app)
 api = Api(app)
 
+
 class Links(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    #slug = db.Column(db.String, unique=True)
+    slug = db.Column(db.String, unique=True)
     url = db.Column(db.String)
 
     def __init__(self, url):
         self.url = url
 
+    def slughash(self, key):
+        return str(key)
+
     @property
-    def redirect_url(self):
+    def slug_url(self):
         return api.url_for(Redirector,
-                           redirect_id=self.id,
+                           slug=self.slug,
                            _external=True)
-    
+
     def serialize(self):
         return {'id': self.id,
                 'url': self.url,
-                'redirect_url': self.redirect_url}
-            
+                'slug': self.slug,
+                'slug_url': self.slug_url}
+
     @staticmethod
     def get_or_create(url):
         result = Links.query.filter_by(url=url).first()
-        created = False
-        
+
         if not result:
-            result, created = Links(url), True
+            result = Links(url)
             db.session.add(result)
             db.session.commit()
+            result.slug = result.slughash(result.id)
+            db.session.commit()
 
-        return result, created
+        return result
+
 
 class Redirector(Resource):
 
-    def get(self, redirect_id):
-        redirect_url = None
-        try:
-            result = Links.query.get_or_404(redirect_id)
-            redirect_url = result.url
-        except NotFound:
-            redirect_url = api.url_for(Viewer, redirect_id=redirect_id)
+    def get(self, slug):
+        # Redirect user to url if available
+        result = Links.query.filter_by(slug=slug).first()
+        if result:
+            return {}, 301, {'Location': result.slug_url}
 
-        return {}, 301, {'Location': redirect_url}
+        # Otherwise redirect to main viewer
+        return {}, 302, {'Location': api.url_for(Viewer, slug=None)}
+
 
 class Viewer(Resource):
 
-    def get(self, redirect_id=None):
-        if redirect_id:
-            redirect_url = None
-            try:
-                result = Links.query.get_or_404(redirect_id)
-                redirect_url = result.url
-            except NotFound:
-                pass
+    def get(self, slug=None):
+        # Show an individual shortened link
+        if slug:
+            result = Links.query.filter_by(slug=slug).first()
+            if result:
+                return result.serialize()
+            else:
+                return {}, 302, {'Location': api.url_for(Viewer, slug=None)}
 
-            return {'redirect_id':redirect_id,
-                    'redirect_url': redirect_url,
-                    'slnky_url': result.redirect_url if redirect_url else None,}
-        else:
-            return [link.serialize() for link in Links.query.all()]
+        # Show all links
+        return [link.serialize() for link in Links.query.all()]
 
 
-class Slnky(Resource):
+class Creator(Resource):
 
     def get(self, url):
-        result, created = Links.get_or_create(url)
-        return {}, 301, {'Location': api.url_for(Viewer, redirect_id=result.id)}
-    
-    def put(self, url):
-        result, created = Links.get_or_create(url)
-        return {'slnky_url': api.url_for(Redirector, redirect_id=result.id, _external=True)}
+        # On a get request we want to show the viewer
+        result = Links.get_or_create(url)
+        return {}, 301, {'Location': api.url_for(Viewer, slug=result.slug)}
 
-api.add_resource(Redirector, '/<int:redirect_id>')
-api.add_resource(Viewer, '/v', '/v/<int:redirect_id>')
-api.add_resource(Slnky, '/c/<path:url>')
+    def put(self, url):
+        # Return the shortened url on PUT
+        result = Links.get_or_create(url)
+        return {'slug_url': result.slug_url}
+
+api.add_resource(Redirector, '/<string:slug>')
+api.add_resource(Viewer, '/v', '/v/<int:slug>')
+api.add_resource(Creator, '/c/<path:url>')
+
+
+@app.before_first_request
+def create_db():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
-
-
